af3c0bb5990c72c0fd8c47a9b7e8565be2324dcb
jainpranshu21 <pranshujain80@gmail.com>
2023-04-13 12:07:41 +0530

    checking

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("trying to connect");
+        System.out.println("checking");
     }
 }
